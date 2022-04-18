6766b7c58416a1a57b67150bcf73a20cc027c104
malev.dg <malev.dg@outlook.com>
2022-04-18 13:03:00 -0500

    registros en google

--- src/firebase/authFunctions.js
+++ src/firebase/authFunctions.js
@@ -1,37 +1,63 @@
 import { app } from "./firebaseConfig.js";
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+import { onNavigate } from '../main.js';
 
 const auth = getAuth();
 
 export const registerWithEmail = (loginEmail,loginPassword) => {
-  console.log(loginEmail,loginPassword)
+//console.log(loginEmail,loginPassword)
 createUserWithEmailAndPassword(auth, loginEmail, loginPassword)
   .then((userCredential) => {
     // Signed in
     const user = userCredential.user;
     console.log(user)
+    onNavigate('/');
   })
   .catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
-    console.log(errorMessage)
+    //console.log(errorMessage)
+    //onNavigate('/');
   });
 };
 
-export const signInWithGoogle = () => {
-  const provider = new GoogleAuthProvider();
-  const auth = getAuth();
-  signInWithPopup(auth, provider)
-    .then((result) => {
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      const user = result.user;
-      onNavigate('/login');
-    }).catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      const email = error.email;
-      const credential = GoogleAuthProvider.credentialFromError(error);
-      console.log(errorCode, errorMessage, email, credential);
-    });
+export const loginGoogle = () => {
+const provider = new GoogleAuthProvider();
+signInWithPopup(auth, provider)
+  .then((result) => {
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    const user = result.user;
+    onNavigate('/login');
+    return user;
+  }).catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    const email = error.email;
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    onNavigate('/login');
+  });
+};
+
+export const signInWithEmail = (loginEmail,loginPassword) => {
+return signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    onAuthStateChanged(auth, (user) => {
+        const correctEmail = user.correctEmail;
+        const uid = user.uid;
+        if (correctEmail) {
+          console.log("hola")
+          onNavigate('/login');
+        }
+        else {
+          onNavigate('/');
+        }
+});
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorCode, errorMessage)
+  });
 };
