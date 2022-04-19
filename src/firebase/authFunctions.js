57ce25e5708c085907700995968dd70b864fc33d
arelia10 <arelia101195@gmail.com>
2022-04-18 19:11:49 -0500

    Merge branch 'main' of https://github.com/arelia10/LIM017-social-network

--- src/firebase/authFunctions.js
+++ src/firebase/authFunctions.js
@@ -1,63 +1,37 @@
 import { app } from "./firebaseConfig.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
-import { onNavigate } from '../main.js';
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
 
 const auth = getAuth();
 
 export const registerWithEmail = (loginEmail,loginPassword) => {
-//console.log(loginEmail,loginPassword)
+  console.log(loginEmail,loginPassword)
 createUserWithEmailAndPassword(auth, loginEmail, loginPassword)
   .then((userCredential) => {
     // Signed in
     const user = userCredential.user;
     console.log(user)
-    onNavigate('/');
   })
   .catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
-    //console.log(errorMessage)
-    //onNavigate('/');
+    console.log(errorMessage)
   });
 };
 
-export const loginGoogle = () => {
-const provider = new GoogleAuthProvider();
-signInWithPopup(auth, provider)
-  .then((result) => {
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-    const user = result.user;
-    onNavigate('/login');
-    return user;
-  }).catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    const email = error.email;
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    onNavigate('/login');
-  });
-};
-
-export const signInWithEmail = (loginEmail,loginPassword) => {
-return signInWithEmailAndPassword(auth, loginEmail, loginPassword)
-  .then((userCredential) => {
-    const user = userCredential.user;
-    onAuthStateChanged(auth, (user) => {
-        const correctEmail = user.correctEmail;
-        const uid = user.uid;
-        if (correctEmail) {
-          console.log("hola")
-          onNavigate('/login');
-        }
-        else {
-          onNavigate('/');
-        }
-});
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorCode, errorMessage)
-  });
+export const signInWithGoogle = () => {
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+      onNavigate('/login');
+    }).catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.email;
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      console.log(errorCode, errorMessage, email, credential);
+    });
 };
