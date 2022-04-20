a10a4ad5e88aebcfe7fe824400516c77ef10a973
arelia10 <arelia101195@gmail.com>
2022-04-20 16:37:01 -0500

    a

--- src/firebase/authFunctions.js
+++ src/firebase/authFunctions.js
@@ -1,37 +1,85 @@
 import { app } from "./firebaseConfig.js";
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+import { onNavigate } from '../main.js';
 
 const auth = getAuth();
 
 export const registerWithEmail = (loginEmail,loginPassword) => {
-  console.log(loginEmail,loginPassword)
-createUserWithEmailAndPassword(auth, loginEmail, loginPassword)
+//console.log(loginEmail,loginPassword)
+const auth = getAuth();
+ return createUserWithEmailAndPassword(auth, loginEmail, loginPassword)
   .then((userCredential) => {
     // Signed in
     const user = userCredential.user;
     console.log(user)
+    onNavigate('/');
   })
   .catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
-    console.log(errorMessage)
+    //console.log(errorMessage)
+    //onNavigate('/');
+    return errorMessage;
   });
 };
 
-export const signInWithGoogle = () => {
-  const provider = new GoogleAuthProvider();
-  const auth = getAuth();
-  signInWithPopup(auth, provider)
-    .then((result) => {
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      const user = result.user;
-      onNavigate('/login');
-    }).catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      const email = error.email;
-      const credential = GoogleAuthProvider.credentialFromError(error);
-      console.log(errorCode, errorMessage, email, credential);
-    });
+export const loginGoogle = () => {
+const provider = new GoogleAuthProvider();
+signInWithPopup(auth, provider)
+  .then((result) => {
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    const user = result.user;
+    onNavigate('/login');
+    return user;
+  }).catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    const email = error.email;
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    onNavigate('/login');
+  });
+};
+
+export const signInWithEmail = (loginEmail,loginPassword,alertEmail) => {
+  const auth1 =  getAuth();
+ signInWithEmailAndPassword(auth1, loginEmail, loginPassword)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    onAuthStateChanged(auth1, (user) => {
+        const correctEmail = user.correctEmail;
+        const uid = user.uid;
+        if (correctEmail) {
+          
+          onNavigate('/');
+        }
+        else {
+          wrongEmail.innerText = 'Verifique su email';
+        }
+});
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    switch (errorCode) {
+      case 'auth/user-not-found':
+        wrongEmail.innerText = 'No existe usuario registrado con ese correo';
+        break;
+      case 'auth/wrong-password':
+        wrongEmail.innerText = 'La contraseña no es válida, intente de nuevo.';
+        break;
+      case 'auth/internal-error':
+        wrongEmail.innerText = 'Digite correctamente su contraseña';
+        break;
+      case 'auth/invalid-email':
+        wrongEmail.innerText = 'Digite correctamente su correo y contraseña';
+        break;
+      case 'auth/email-already-exists':
+        wrongEmail.innerText = 'Este correo ya esta registrado, intente de nuevo.';
+        break;
+      default:
+        wrongEmail.innerText = errorCode;
+    }
+  });
+return result;
 };
