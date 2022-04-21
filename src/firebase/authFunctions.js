dcd95e9094ef9de5f108327514ae083b8d5c5c53
malev.dg <malev.dg@outlook.com>
2022-04-21 10:16:06 -0500

    registro firestore

--- src/firebase/authFunctions.js
+++ src/firebase/authFunctions.js
@@ -1,24 +1,55 @@
 import { app } from "./firebaseConfig.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
 import { onNavigate } from '../main.js';
-import { db } from "./firebaseConfig.js";
+//import { db } from "./firebaseConfig.js";
 
 const auth = getAuth();
+export const db = getFirestore();
 
-export const registerWithEmail = (loginEmail,loginPassword) => {
+export const registerWithEmail = (loginEmail, loginPassword, loginName) => {
+  createUserWithEmailAndPassword(auth, loginEmail, loginPassword)
+   .then(() => {
+   try {
+    const RegisterTellMe =  addDoc(collection(db, "RegisterTellMe"), {
+      newNickName:loginName,
+      newEmail:loginEmail,
+    });
+    console.log("Registro realizado ", RegisterTellMe.nickName);
+
+  }
+  catch (e){
+    console.error("Error, no se pudo registrar ", e);
+  }
+})
+   .catch((error) => {
+     const errorCode = error.code;
+     const errorMessage = error.message;
+     alert("errorMessage");
+   });
+
 //console.log(loginEmail,loginPassword)
-createUserWithEmailAndPassword(auth, loginEmail, loginPassword)
+/*createUserWithEmailAndPassword(auth,loginEmail,loginPassword)
   .then((userCredential) => {
+    try {
+    const RegisterTellMe = addDoc(collection(db,"RegisterTellMe"), {
+      nickName:nickName,
+      email:loginEmail,
+    });
+    console.log("Registro realizado");
     const user = userCredential.user;
     console.log(user)
     onNavigate('/');
-  })
+  }
+  catch (e){
+  console.error("Error, no se pudo registrar", e);
+  }})
   .catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
     //console.log(errorMessage)
     //onNavigate('/');
-  });
+  });*/
 };
 
 export const signInWithEmail = (loginEmail,loginPassword) => {
--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
+//import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
 
 
 const firebaseConfig = {
@@ -12,4 +12,4 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+//export const db = getFirestore(app);
